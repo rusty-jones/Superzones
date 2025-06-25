0d0fe304423e435c815424771806f50ebf404cd4
rusty-jones <prateek101270cse@gmail.com>
2025-06-25 13:28:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ ticker_mapping = {
 # Timeframe and period mapping
 timeframes = {
     "1m": "1m", "5m": "5m", "15m": "15m", "30m": "30m",
-    "1h": "1h", "4h": "4h", "1d": "1d", "1wk": "1wk", "1mo": "1mo"
+    "1h": "1h", "4h": "4h", "1d": "1d", "1wk": "1wk", "1mo": "1mo","3mo":"3mo"
 }
 periods = ["1d", "5d", "1mo", "3mo", "6mo", "1y","2y","5y","10y","ytd","max"]
 
